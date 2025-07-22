c88a97528fb58373fff2a5f2db57435d63ec9a01
Takudzwa Stanley Maguma <161417881+magmastormy@users.noreply.github.com>
2025-07-22 10:34:33 -0400

    idk what i did but lets start from this change

---
+++ src/components/Settings/settingsManager.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SettingsManager = () => {
+    return (
+        <div>
+            <h1>Settings Manager</h1>
+        </div>
+    );
+};
+
+export default SettingsManager;
